b03cd4655af932d68542b569483e6801ff220664
jameslee0106 <leejames0106@gmail.com>
2023-01-19 13:06:12 -0800

    Recreating an Airbnb Experiences page using React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import Data from "./data"
 
 export default function App() {
-  return (
-    <h1>App component</h1>
-  )
+    
+    const cards = Data.map(item => {
+        return (
+            <Card 
+                key ={item.id}
+                item = {item} //{...item} works too 
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
 }
-
-
